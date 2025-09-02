6c3689c8babb7682c1a7fed108bef7563ce8c6e0
KARINA AUDAY <karina.auday@ort.edu.ar>
2025-09-01 22:35:37 -0300

    Creo la clase Persona y su clases hereadas

---
+++ 2025-2C-EstacionamietoORT/Models/Cliente.cs
@@ -0,0 +1,8 @@
+﻿namespace _2025_2C_EstacionamietoORT.Models
+{
+    public class Cliente : Persona
+    {
+        public string Cuit { get; set; }
+
+    }
+}
---
+++ 2025-2C-EstacionamietoORT/Models/Persona.cs
@@ -0,0 +1,22 @@
+﻿namespace _2025_2C_EstacionamietoORT.Models
+{
+    public class Persona
+    {
+        public int Id { get; set; }
+        public string Nombre { get; set; }
+        public string Apellido { get; set; }
+
+        public string Dni { get; set; }
+
+        public string Email { get; set; }
+
+        public string UserName { get; set; }
+
+        public string Telefono { get; set; }
+
+        public DateTime FechaAlta   { get; set; }
+
+        public string Direccion { get; set; }
+
+    }
+}
